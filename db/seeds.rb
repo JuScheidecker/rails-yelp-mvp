b9b460c99931b8bb1114893a35e2b332ea9712bd
Juliette Scheidecker <juscheidecker@gmail.com>
2017-05-17 17:34:34 +0200

    seed 5 restos

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+Restaurant.destroy_all
+
 Restaurant.create!({
   name: "Le Dindon en Laisse",
   address: "18 Rue Beautreillis, 75004 Paris, France",
@@ -9,7 +11,7 @@ Restaurant.create!({
 Restaurant.create!({
   name: "patatra",
   address: "5 rue jaune",
-  phone_number: "01 20 34 00 00 00"
+  phone_number: "01 20 34 00 00 00",
   category: "belgian",
   review: "5"
 })
@@ -17,7 +19,7 @@ Restaurant.create!({
 Restaurant.create!({
   name: "le temple de l'Asie",
   address: "5 rue rouge",
-  phone_number: "01 20 34 00 00 01"
+  phone_number: "01 20 34 00 00 01",
   category: "chinese",
   review: "4"
 })
@@ -25,7 +27,7 @@ Restaurant.create!({
 Restaurant.create!({
   name: "antipasti",
   address: "2 rue verte",
-  phone_number: "01 20 34 00 00 02"
+  phone_number: "01 20 34 00 00 02",
   category: "italian",
   review: "2"
 })
@@ -33,7 +35,7 @@ Restaurant.create!({
 Restaurant.create!({
   name: "la dolce vita",
   address: "5 rue bleue",
-  phone_number: "01 20 34 00 00 02"
+  phone_number: "01 20 34 00 00 02",
   category: "italian",
   review: "4"
 })
